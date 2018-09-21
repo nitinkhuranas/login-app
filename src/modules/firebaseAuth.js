363a1d159ca145062921a4eabadaaa10343dacb7
nitinkhuranas <nitinkhuranas@gmail.com>
2018-09-21 03:19:01 +0000

    update firebase api key

--- src/modules/firebaseAuth.js
+++ src/modules/firebaseAuth.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 var config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+     apiKey: "AIzaSyDyTceYBrx-9Scf4J4syNKyGu9mc95Gxgg",
+    authDomain: "sublime-state-146411.firebaseapp.com",
+    databaseURL: "https://sublime-state-146411.firebaseio.com",
+    projectId: "sublime-state-146411",
+    storageBucket: "sublime-state-146411.appspot.com",
+    messagingSenderId: "571825557320"
   };
 
 firebase.initializeApp(config);
